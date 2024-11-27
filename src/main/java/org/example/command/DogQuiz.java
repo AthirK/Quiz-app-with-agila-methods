1ac909b74fd61d71f49b85b93a5f2309953ed610
AthirK <athirkazem@gmail.com>
2024-11-27 18:57:05 +0100

    Revert "add more questions and input options."

--- src/main/java/org/example/command/DogQuiz.java
+++ src/main/java/org/example/command/DogQuiz.java
@@ -14,35 +14,19 @@ public class DogQuiz extends Command {
     @Override
     public void execute(String[] commandArgs) {
 
-        //Welcome and art
-        System.out.println("                            __");
-        System.out.println("     ,                    ,\" e`--o");
-        System.out.println("    ((                   (  | __,'");
-        System.out.println("     \\\\~----------------' \\_;/");
-        System.out.println("     (     # DOG QUIZ #     /");
-        System.out.println("     /) ._______________.  )");
-        System.out.println("    (( (               (( (");
-        System.out.println("     ``-'               ``-'");
-        System.out.println();
-
-
         Scanner scan = new Scanner(System.in);
         int score = 0;
 
         // Question 1
         System.out.println("What is the most common dog in Sweden?");
-        System.out.println("a) Labrador\nb) Whippet\nc) German shepherd");
+        System.out.println("a) Labrador\nb) Whippet\nc) A mix of american bully and blood hound");
         String input1 = scan.nextLine();
         if (input1.equalsIgnoreCase("a")) {
             System.out.println("Correct!");
             score++;
             System.out.println();
-        } else if (input1.equalsIgnoreCase("Labrador")){
-            System.out.println("Correct!");
-            score++;
-            System.out.println();
         } else {
-            System.out.println("Wrong, the correct answer is Labrador.");
+            System.out.println("Wrong! The correct answer is a) Labrador.");
         }
 
         // Question 2
@@ -53,12 +37,8 @@ public class DogQuiz extends Command {
             System.out.println("Correct!");
             score++;
             System.out.println();
-        } else if (input1.equalsIgnoreCase("4")){
-            System.out.println("Correct!");
-            score++;
-            System.out.println();
         } else {
-            System.out.println("Wrong, the correct answer is 4");
+            System.out.println("Wrong! The correct answer is a) 4 legs");
         }
 
         // Question 3
@@ -69,51 +49,13 @@ public class DogQuiz extends Command {
             System.out.println("Correct!");
             score++;
             System.out.println();
-        } else if (input1.equalsIgnoreCase("they eat grass")){
-            System.out.println("Correct!");
-            score++;
-            System.out.println();
         } else {
-            System.out.println("Wrong, the correct answer is: c) They eay grass");
+            System.out.println("Wrong! The correct answer is c) They eay grass");
         }
 
-
-        // Question 4
-        System.out.println("Which sense is most developed in dogs??");
-        System.out.println("a) Sight \nb) Smell \nc) Hearing");
-        String input4 = scan.nextLine();
-        if (input3.equalsIgnoreCase("b")) {
-            System.out.println("Correct!");
-            score++;
-            System.out.println();
-        } else if (input1.equalsIgnoreCase("Smell")){
-            System.out.println("Correct!");
-            score++;
-            System.out.println();
-        } else {
-            System.out.println("Wrong, the correct answer is: Smell");
-        }
-
-
-        // Question 5
-        System.out.println("What is a group of puppies called?");
-        System.out.println("a) Pack\nb) Litter\nc) Herd");
-        String input5 = scan.nextLine();
-        if (input3.equalsIgnoreCase("b")) {
-            System.out.println("Correct!");
-            score++;
-            System.out.println();
-        } else if (input1.equalsIgnoreCase("Litter")){
-            System.out.println("Correct!");
-            score++;
-            System.out.println();
-        } else {
-            System.out.println("Wrong, the correct answer is: Litter");
-        }
-
-        // Displays the final score
-        System.out.println("\nYou scored " + score + " out of 5!");
-
-        System.out.println("The dog quiz is finished, type 'help' to see menu.");
+        // Display the final score
+        System.out.println("\nYou scored " + score + " out of 3!");
     }
+
 }
+
